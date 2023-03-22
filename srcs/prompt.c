6dcaa0711269327b73a1378a0385d17fbbb200e6
romann-broque <kinoide56@gmail.com>
2023-03-22 11:29:18 +0100

    libft uses gcc now + prompt file modified

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:52:07 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/21 18:29:36 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/03/22 11:23:32 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 static bool	get_command(void)
 {
 	char *const	command = get_next_line(STDIN_FILENO);
-
+	const char	eof[] = {EOF, '\0'};
+	printf("\n[%s]\n", command);
+	printf("\n[%s]\n", eof);
+	if (command == NULL)
+		exit(EXIT_SUCCESS);
 	free(command);
 	return (command != NULL);
 }
