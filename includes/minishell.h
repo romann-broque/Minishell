618c286b9ebffad15b4aa7c98e999ba12dd5d567
Romann BROQUE <58867296+romann-broque@users.noreply.github.com>
2023-03-23 10:50:19 +0100

    Mve1 us5 exit b UI ltin (#7)
    * Added valgrind
    
    * to update the libft
    
    * added the exit capality
    
    * trim line
    
    * prompt folder
    
    * tester folder
    
    * valgrind suppress file
    
    ---------
    
    Co-authored-by: HelloThere-ImMat <dorr.mathias@gmail.com>

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:54:10 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/22 12:40:26 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/03/22 16:50:08 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,17 @@
 /// DEFINES ///
 ///////////////
 
-# define PROMPT "minishell $ "
-# define EXIT_MESSAGE "exit"
+# define PROMPT			"minishell $ "
+# define EXIT_MESSAGE	"exit"
+# define WHITESPACES	" \t\n\v\f\r"
+# define SEPARATORS		" \t\n"
+# define LAST_RETVAL	EXIT_SUCCESS // to put in a global variable to keep it between commands
 
 /////////////////
 /// FUNCTIONS ///
 /////////////////
 
-// exit_print.c
+// exit_shell.c
 
 void	exit_shell(const int exit_value);
 
--- srcs/prompt/prompt.c
+++ srcs/prompt/prompt.c
@@ -6,18 +6,28 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:52:07 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/22 12:41:02 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/03/23 10:41:45 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	exec_command(const char *command)
+{
+	if (streq(command, "exit"))
+		exit_shell(LAST_RETVAL);
+}
+
 static void	get_command(void)
 {
-	char *const	command = readline(PROMPT);
+	char *const	line = readline(PROMPT);
+	char *const	command = ft_strtrim(line, SEPARATORS);
 
+	free(line);
 	if (command == NULL)
-		exit_shell(EXIT_SUCCESS);
+		exit_shell(LAST_RETVAL);
+	else
+		exec_command(command);
 	free(command);
 }
 
