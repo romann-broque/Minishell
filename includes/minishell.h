c03ef9c4824a60e5fbe40651085b8693eaafd78b
HelloThere-ImMat <88711075+HelloThere-ImMat@users.noreply.github.com>
2023-03-23 15:37:37 +0100

    Mve1 us6 ctrl c (#8)
    * started the signal CTRL-C
    
    * sorry for the supplementary files
    
    * Normed
    
    * CTR-C finished
    
    * Chadded that shit

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mat <mat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:54:10 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/22 16:50:08 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/03/23 14:31:57 by mat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include "libft.h"
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <signal.h>
 
 ///////////////
 /// DEFINES ///
@@ -29,7 +30,7 @@
 # define EXIT_MESSAGE	"exit"
 # define WHITESPACES	" \t\n\v\f\r"
 # define SEPARATORS		" \t\n"
-# define LAST_RETVAL	EXIT_SUCCESS // to put in a global variable to keep it between commands
+# define LAST_RETVAL	EXIT_SUCCESS
 
 /////////////////
 /// FUNCTIONS ///
@@ -42,5 +43,9 @@ void	exit_shell(const int exit_value);
 // prompt.c
 
 void	prompt(void);
+
+// signal.c
+
+void	set_catcher(void);
 
 #endif
--- srcs/signal/signal.c
+++ srcs/signal/signal.c
@@ -1,39 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   prompt.c                                           :+:      :+:    :+:   */
+/*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mat <mat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/21 15:52:07 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/23 14:30:01 by mat              ###   ########.fr       */
+/*   Created: 2023/03/23 09:54:40 by mat               #+#    #+#             */
+/*   Updated: 2023/03/23 14:31:29 by mat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	exec_command(const char *command)
+static void	handle_sigint(__attribute__((unused)) int signal)
 {
-	if (streq(command, "exit"))
-		exit_shell(LAST_RETVAL);
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
-static void	get_command(void)
+void	set_catcher(void)
 {
-	char *const	line = readline(PROMPT);
-	char *const	command = ft_strtrim(line, SEPARATORS);
-
-	free(line);
-	if (command == NULL)
-		exit_shell(LAST_RETVAL);
-	else
-		exec_command(command);
-	free(command);
-}
-
-void	prompt(void)
-{
-	set_catcher();
-	while (true)
-		get_command();
+	signal(SIGINT, handle_sigint);
 }
