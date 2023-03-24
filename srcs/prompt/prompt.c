5f79858126d99a67203a8e57448fd7b7a19ba3bc
HelloThere-ImMat <88711075+HelloThere-ImMat@users.noreply.github.com>
2023-03-24 16:13:25 +0100

    Mve2 us1 lexer st1 split (#10)
    * Added the split
    
    * Added missing separators, changed command to const, added [] to print_command and normed
    
    * changes after review

--- srcs/prompt/prompt.c
+++ srcs/prompt/prompt.c
@@ -6,29 +6,30 @@
 /*   By: mat <mat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:52:07 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/23 14:30:01 by mat              ###   ########.fr       */
+/*   Updated: 2023/03/24 15:10:34 by mat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	exec_command(const char *command)
+static void	exec_command(char **const token_array)
 {
-	if (streq(command, "exit"))
+	if (streq(token_array[0], "exit"))
 		exit_shell(LAST_RETVAL);
 }
 
 static void	get_command(void)
 {
-	char *const	line = readline(PROMPT);
-	char *const	command = ft_strtrim(line, SEPARATORS);
+	char *const		line = readline(PROMPT);
+	char **const	token_array = ft_split_set(line, SEPARATORS);
 
 	free(line);
-	if (command == NULL)
+	if (token_array == NULL)
 		exit_shell(LAST_RETVAL);
 	else
-		exec_command(command);
-	free(command);
+		exec_command(token_array);
+	print_command(token_array);
+	free_strs(token_array);
 }
 
 void	prompt(void)
