53557ba839e99718548b939b981628a226e696ae
romann-broque <kinoide56@gmail.com>
2023-04-03 14:53:53 +0200

    test

--- tests/CUNIT/srcs/minishell.test.c
+++ tests/CUNIT/srcs/minishell.test.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 22:28:05 by rbroque           #+#    #+#             */
-/*   Updated: 2023/04/03 11:57:32 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/04/03 14:52:12 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ void	set_tests(void)
 		},
 		{.suite = NULL, .sname = NULL}
 	};
-
 
 	map_suite(suite_mapping);
 }
