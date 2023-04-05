5dae9ba31521a89f02a68e93149625f935be9340
HelloThere-ImMat <dorr.mathias@gmail.com>
2023-04-05 15:04:17 +0200

    free the expand var returns

--- tests/CUNIT/srcs/tests/expand_var.test.c
+++ tests/CUNIT/srcs/tests/expand_var.test.c
@@ -6,11 +6,17 @@
 /*   By: mat <mat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 11:47:05 by mat               #+#    #+#             */
-/*   Updated: 2023/04/05 14:32:18 by mat              ###   ########.fr       */
+/*   Updated: 2023/04/05 15:03:26 by mat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cunit.test.h"
+
+void	ASSERT_AND_FREE(char *str1, const char *str2)
+{
+	CU_ASSERT_STRING_EQUAL(str1, str2);
+	free(str1);
+}
 
 void	expand_var__test(void)
 {
@@ -53,23 +59,23 @@ void	expand_var__test(void)
 	const char	str_in_t19[] = "$    $";
 	const char	str_ref_t19[] = "$    $";
 
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t1), str_ref_t1);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t2), str_ref_t2);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t3), str_ref_t3);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t4), str_ref_t4);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t5), str_ref_t5);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t6), str_ref_t6);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t7), str_ref_t7);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t8), str_ref_t8);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t9), str_ref_t9);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t10), str_ref_t10);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t11), str_ref_t11);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t12), str_ref_t12);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t13), str_ref_t13);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t14), str_ref_t14);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t15), str_ref_t15);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t16), str_ref_t16);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t17), str_ref_t17);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t18), str_ref_t18);
-	CU_ASSERT_STRING_EQUAL(expand_var(str_in_t19), str_ref_t19);
+	ASSERT_AND_FREE(expand_var(str_in_t1), str_ref_t1);
+	ASSERT_AND_FREE(expand_var(str_in_t2), str_ref_t2);
+	ASSERT_AND_FREE(expand_var(str_in_t3), str_ref_t3);
+	ASSERT_AND_FREE(expand_var(str_in_t4), str_ref_t4);
+	ASSERT_AND_FREE(expand_var(str_in_t5), str_ref_t5);
+	ASSERT_AND_FREE(expand_var(str_in_t6), str_ref_t6);
+	ASSERT_AND_FREE(expand_var(str_in_t7), str_ref_t7);
+	ASSERT_AND_FREE(expand_var(str_in_t8), str_ref_t8);
+	ASSERT_AND_FREE(expand_var(str_in_t9), str_ref_t9);
+	ASSERT_AND_FREE(expand_var(str_in_t10), str_ref_t10);
+	ASSERT_AND_FREE(expand_var(str_in_t11), str_ref_t11);
+	ASSERT_AND_FREE(expand_var(str_in_t12), str_ref_t12);
+	ASSERT_AND_FREE(expand_var(str_in_t13), str_ref_t13);
+	ASSERT_AND_FREE(expand_var(str_in_t14), str_ref_t14);
+	ASSERT_AND_FREE(expand_var(str_in_t15), str_ref_t15);
+	ASSERT_AND_FREE(expand_var(str_in_t16), str_ref_t16);
+	ASSERT_AND_FREE(expand_var(str_in_t17), str_ref_t17);
+	ASSERT_AND_FREE(expand_var(str_in_t18), str_ref_t18);
+	ASSERT_AND_FREE(expand_var(str_in_t19), str_ref_t19);
 }
