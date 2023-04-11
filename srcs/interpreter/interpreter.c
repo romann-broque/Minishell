2e8b86ee026bdcc41f1a1717e29c2f76588324ba
romann-broque <kinoide56@gmail.com>
2023-04-11 22:23:25 +0200

    tests for interpreter

--- srcs/interpreter/interpreter.c
+++ srcs/interpreter/interpreter.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 14:20:19 by mat               #+#    #+#             */
-/*   Updated: 2023/04/11 17:07:24 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/04/11 22:16:22 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,18 @@ t_command	*init_command(t_list *tokens, const char **env)
 	return (cmd_data);
 }
 
+static void	skip_generic(t_list **tokens)
+{
+	t_toktype	token_type;
+
+	token_type = get_type_from_tok((*tokens)->content);
+	while (token_type == T_GENERIC && token_type != T_END)
+	{
+		*tokens = (*tokens)->next;
+		token_type = get_type_from_tok((*tokens)->content);
+	}
+}
+
 static void	skip_until_generic(t_list **tokens)
 {
 	t_toktype	token_type;
@@ -46,8 +58,14 @@ t_list	*interpreter(t_list *tokens, const char **env)
 	t_command	*cmd_data;
 	t_list		*commands;
 
+	commands = NULL;
 	skip_until_generic(&tokens);
-	cmd_data = init_command(tokens, env);
-	commands = ft_lstnew(cmd_data);
+	while (get_type_from_tok(tokens->content) != T_END)
+	{
+		cmd_data = init_command(tokens, env);
+		ft_lstadd_back(&commands, ft_lstnew(cmd_data));
+		skip_generic(&tokens);
+		skip_until_generic(&tokens);
+	}
 	return (commands);
 }
--- tests/CUNIT/srcs/tests/interpreter.test.c
+++ tests/CUNIT/srcs/tests/interpreter.test.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 17:44:29 by rbroque           #+#    #+#             */
-/*   Updated: 2023/04/11 19:31:05 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/04/11 22:23:04 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,6 @@
 #define MAX_LEN_CMD 100
 
 extern const char	**g_env;
-
-// static void	free_command_arg(t_command *cmd)
-// {
-// 	if (cmd != NULL)
-// 		free_strs(cmd->command);
-// }
 
 static t_list	*get_list_from_array(t_token *array)
 {
@@ -38,40 +32,40 @@ static t_list	*get_list_from_array(t_token *array)
 	return (tokens_list);
 }
 
-static t_list	*get_list_from_array_cmd(t_command *array)
+static t_list	*get_list_from_array_cmd(t_command *array, const size_t size)
 {
 	size_t	i;
 	t_list	*cmd_list;
 
 	cmd_list = NULL;
 	i = 0;
-	while (array[i].command[0] != NULL)
+	while (i < size)
 	{
 		ft_lstadd_back(&cmd_list, ft_lstnew(array + i));
 		i++;
 	}
-	ft_lstadd_back(&cmd_list, ft_lstnew(array + i));
 	return (cmd_list);
 }
 
 static bool	strs_eq(const char **strs1, const char **strs2)
 {
-	while (strs1 != NULL && strs2 != NULL
+	while (*strs1 != NULL && *strs2 != NULL
 		&& streq(*strs1, *strs2) == true)
 	{
 		++strs1;
 		++strs2;
 	}
-	return (strs1 == NULL && strs2 == NULL);
+	return (*strs1 == NULL && *strs2 == NULL);
 }
 
 static bool	is_same_cmd(t_command *cmd1, t_command *cmd2)
 {
-	return (cmd1 != NULL && cmd2 != NULL
+	return ((cmd1 == NULL && cmd2 == NULL)
+		|| (cmd1 != NULL && cmd2 != NULL
 		&& strs_eq((const char **)cmd1->command, (const char **)cmd2->command) == true
 		&& strs_eq(cmd1->env, cmd2->env) == true
 		&& cmd1->fdin == cmd2->fdin
-		&& cmd1->fdout == cmd2->fdout
+		&& cmd1->fdout == cmd2->fdout)
 	);
 }
 
@@ -86,10 +80,10 @@ static bool	comp_commands(t_list *cmd_out, t_list *cmd_exp)
 	return (cmd_out == NULL && cmd_exp == NULL);
 }
 
-static bool	is_cmd_valid(t_token *tok_array, t_command *cmd_array)
+static bool	is_cmd_valid(t_token *tok_array, t_command *cmd_array, const size_t size_cmd_array)
 {
 	t_list	*tok_list = get_list_from_array(tok_array);
-	t_list	*cmd_exp = get_list_from_array_cmd(cmd_array);
+	t_list	*cmd_exp = get_list_from_array_cmd(cmd_array, size_cmd_array);
 	t_list	*cmd_out = interpreter(tok_list, g_env);
 	bool	ret_val = comp_commands(cmd_out, cmd_exp);
 
@@ -111,15 +105,76 @@ void	interpreter__test(void)
 	t_command	cmds1[] = {
 		{.command = (char *[]){
 			"hola",
-			"como",
-			"esta"
+			"commo",
+			"esta",
+			NULL
 			},
 		.env = g_env,
 		.fdin = STDIN_FILENO,
 		.fdout = STDOUT_FILENO,
 		},
-		{.command = (char *[]){NULL}}
+	};
+	
+	t_token	toks2[] = {
+		{.type = T_START, .value = NULL},
+		{.type = T_END, .value = NULL}
+	};
+	t_command	*cmds2 = NULL;
+	t_token	toks3[] = {
+		{.type = T_START, .value = NULL},
+		{.type = T_GENERIC, .value = "ls"},
+		{.type = T_GENERIC, .value = "srcs/"},
+		{.type = T_PIPE, .value = PIPE},
+		{.type = T_GENERIC, .value = "cat"},
+		{.type = T_GENERIC, .value = "lol"},
+		{.type = T_END, .value = NULL}
+	};
+	t_command	cmds3[] = {
+		{.command = (char *[]){
+			"ls",
+			"srcs/",
+			NULL
+			},
+		.env = g_env,
+		.fdin = STDIN_FILENO,
+		.fdout = STDOUT_FILENO,
+		},
+		{.command = (char *[]){
+			"cat",
+			"lol",
+			NULL
+			},
+		.env = g_env,
+		.fdin = STDIN_FILENO,
+		.fdout = STDOUT_FILENO,
+		},
+	};
+	t_token	toks4[] = {
+		{.type = T_START, .value = NULL},
+		{.type = T_GENERIC, .value = "ls"},
+		{.type = T_GENERIC, .value = "srcs/"},
+		{.type = T_GENERIC, .value = "|"},
+		{.type = T_GENERIC, .value = "cat"},
+		{.type = T_GENERIC, .value = "lol"},
+		{.type = T_END, .value = NULL}
+	};
+	t_command	cmds4[] = {
+		{.command = (char *[]){
+			"ls",
+			"srcs/",
+			"|",
+			"cat",
+			"lol",
+			NULL
+			},
+		.env = g_env,
+		.fdin = STDIN_FILENO,
+		.fdout = STDOUT_FILENO,
+		},
 	};
 
-	CU_ASSERT_TRUE(is_cmd_valid(toks1, cmds1));
+	CU_ASSERT_TRUE(is_cmd_valid(toks1, cmds1, 1));
+	CU_ASSERT_TRUE(is_cmd_valid(toks2, cmds2, 0));
+	CU_ASSERT_TRUE(is_cmd_valid(toks3, cmds3, 2));
+	CU_ASSERT_TRUE(is_cmd_valid(toks4, cmds4, 1));
 }
