0810924e001d1feaabd3241f92afc8acace256d8
romann-broque <kinoide56@gmail.com>
2023-04-13 18:07:17 +0200

    add free_manager in exit_shell function

--- srcs/exit/exit_shell.c
+++ srcs/exit/exit_shell.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 12:35:37 by rbroque           #+#    #+#             */
-/*   Updated: 2023/04/13 14:48:41 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/04/13 18:06:40 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 void	exit_shell(const int exit_value)
 {
 	ft_dprintf(STDERR_FILENO, "%s\n", EXIT_MESSAGE);
+	free_manager();
 	exit(exit_value);
 }
