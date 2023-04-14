228607ebf9265b2220abb5f54ef6d3934d511f27
HelloThere-ImMat <dorr.mathias@gmail.com>
2023-04-14 11:44:32 +0200

    last change

--- srcs/execution/cmd_path_utils.c
+++ srcs/execution/cmd_path_utils.c
@@ -6,7 +6,7 @@
 /*   By: mat <mat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 09:58:46 by mat               #+#    #+#             */
-/*   Updated: 2023/04/14 11:41:08 by mat              ###   ########.fr       */
+/*   Updated: 2023/04/14 11:43:57 by mat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ bool	is_var_path_in_env(const char **env)
 	size_t	i;
 
 	i = 0;
-	while (env[i] != NULL && ft_strncmp(PATH_VAR, env[i], PATH_VAR_LEN) != 0)
+	while (env[i] != NULL && is_path_var(env[i]) == false)
 		i++;
 	return (env[i] != NULL);
 }
