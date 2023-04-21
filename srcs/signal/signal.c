36ee83a87962c8a6d3b4ffb8958d846a39633ae9
HelloThere-ImMat <dorr.mathias@gmail.com>
2023-04-21 14:02:07 +0200

    started on the CTRLBS

--- srcs/signal/signal.c
+++ srcs/signal/signal.c
@@ -6,7 +6,7 @@
 /*   By: mat <mat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 09:54:40 by mat               #+#    #+#             */
-/*   Updated: 2023/03/23 14:31:29 by mat              ###   ########.fr       */
+/*   Updated: 2023/04/21 13:58:50 by mat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,18 @@ static void	handle_sigint(__attribute__((unused)) int signal)
 	rl_redisplay();
 }
 
+static void	handle_sigquit(__attribute__((unused)) int signal)
+{
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	printf("blata\n");
+	rl_redisplay();
+	return ;
+}
+
 void	set_catcher(void)
 {
 	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, handle_sigquit);
 }
