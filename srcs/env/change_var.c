780af8a403cb539505dea840a240703e1c6ab4d3
romann-broque <kinoide56@gmail.com>
2023-04-21 19:03:02 +0200

    update change_var function

--- srcs/env/change_var.c
+++ srcs/env/change_var.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 09:58:07 by rbroque           #+#    #+#             */
-/*   Updated: 2023/04/21 16:32:48 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/04/21 19:02:05 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,12 +58,17 @@ static void	add_var(const char *var_name, const char *var_value)
 void	change_var(const char *var_name, const char *var_value)
 {
 	const size_t	len = ft_strlen(var_name);
+	size_t			offset;
 	size_t			i;
 
 	i = 0;
-	while (g_global.env[i] != NULL
-		&& ft_strncmp(g_global.env[i], var_name, len) != 0)
+	while (g_global.env[i] != NULL)
+	{
+		offset = abs_index(g_global.env[i], EQUAL_SIGN);
+		if (offset == len && ft_strncmp(g_global.env[i], var_name, offset) == 0)
+			break ;
 		++i;
+	}
 	if (g_global.env[i] != NULL)
 		change_value(g_global.env + i, var_value);
 	else
