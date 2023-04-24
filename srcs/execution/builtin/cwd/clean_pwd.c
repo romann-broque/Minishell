a679a224362f15c1e116210c427e684c2ba9b4ef
romann-broque <kinoide56@gmail.com>
2023-04-24 22:15:48 +0200

    debug in progress

--- srcs/execution/builtin/cwd/clean_pwd.c
+++ srcs/execution/builtin/cwd/clean_pwd.c
@@ -6,25 +6,44 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 17:30:16 by rbroque           #+#    #+#             */
-/*   Updated: 2023/04/24 18:28:15 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/04/24 22:15:39 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*clean_pwd(const char *new_pwd, const char *curr_path)
+static char	*join_path(const char *prefix_path, const char *suffix_path)
+{
+	char	*tmp;
+	char	*join;
+
+	tmp = ft_strjoin(prefix_path, FWD_SLASH_STR);
+	join = ft_strjoin(tmp, suffix_path);
+	free(tmp);
+	return (join);
+}
+
+static char	*join_path_without_backpath(
+	const char *prefix,
+	const char *suffix
+	)
 {
 	char	*new;
 	char	*tmp;
 
-	tmp = ft_strjoin(curr_path, FWD_SLASH_STR);
-	new = ft_strjoin(tmp, new_pwd);
+	tmp = join_path(prefix, suffix);
+	new = rm_backpath(tmp);
 	free(tmp);
-	if (ft_strstr(new, BACKPATH) != NULL)
-		tmp = rm_backpath(new);
-	else
-		tmp = ft_strdup(new);
-	free(new);
+	return (new);
+}
+
+char	*clean_pwd(const char *new_pwd, const char *curr_path)
+{
+	char	*tmp;
+	char	*new;
+
+	tmp = join_path_without_backpath(curr_path, new_pwd);
+	printf("tmp --> [%s]\n", tmp);
 	new = rm_double_slash(tmp);
 	free(tmp);
 	return (new);
