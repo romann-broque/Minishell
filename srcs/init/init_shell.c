53a7d47e57e8dd59532e60bbd3ef57c2cd8c1400
HelloThere-ImMat <dorr.mathias@gmail.com>
2023-05-08 11:15:21 +0200

    fixed env-i issue

--- srcs/init/init_shell.c
+++ srcs/init/init_shell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_shell.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mat <mat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 15:34:28 by rbroque           #+#    #+#             */
-/*   Updated: 2023/05/03 18:40:27 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/05/08 11:09:38 by mat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ t_global	g_global;
 static void	init_pwd(void)
 {
 	char *const	curr_pwd = getcwd(NULL, 0);
+	char		*old_pwd;
 
 	if (curr_pwd != NULL)
 	{
 		update_cwd_var(curr_pwd);
-		if (streq(ft_getenv(OLDPWD_VAR), EMPTY_STR) == true)
+		old_pwd = ft_getenv(OLDPWD_VAR);
+		if (old_pwd != NULL && streq(old_pwd, EMPTY_STR) == true)
 			set_var_flag(OLDPWD_VAR, SLEEP_MASK);
 	}
 	free(curr_pwd);
