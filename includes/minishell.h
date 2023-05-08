e74d43bb0a180e09637d3fe31250b871a9ff4672
HelloThere-ImMat <88711075+HelloThere-ImMat@users.noreply.github.com>
2023-05-08 14:02:48 +0200

    Mve6 fix (#51)
    * fixed env-i issue
    
    * added the incrementation of SHLVL
    
    * small change to expand utils
    
    * applied comments

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mat <mat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 16:58:24 by rbroque           #+#    #+#             */
-/*   Updated: 2023/05/07 15:28:59 by mat              ###   ########.fr       */
+/*   Updated: 2023/05/08 12:09:50 by mat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,7 @@
 # define NEWLINE_STR	"\n"
 # define SPACE_STR		" "
 # define DQUOTE_STR		"\""
+# define SHLVL_DEFAULT	"0"
 
 // builtins
 
@@ -69,6 +70,7 @@
 # define PWD_VAR		"PWD"
 # define PATH_VAR		"PATH"
 # define CDPATH_VAR		"CDPATH"
+# define SHLVL_VAR		"SHLVL"
 
 // tok_string
 
