c1421067b97627964d16df50479ab34d13c67849
romann-broque <kinoide56@gmail.com>
2023-05-08 11:53:59 +0200

    fix some tests with unset PWD cases

--- tests/tester_folder/tester.sh
+++ tests/tester_folder/tester.sh
@@ -70,7 +70,7 @@ function put_format()
 
  echo -e "${BLUE}\n<====  BASH  ====>\n${NC}"
 
-files=( "basic" "builtin_cwd" "echo_builtin" "expansion" "assign" "exit_builtin" "export_builtin")
+files=( "basic" "builtin_cwd" "echo_builtin" "expansion" "assign" "exit_builtin" "export_builtin" "unset")
 
 inputs=($(put_format "$IN_FOLDER" ".in" "${files[@]}"))
 outputs=($(put_format "$OUT_FOLDER" ".out" "${files[@]}"))
