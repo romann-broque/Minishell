e91544bbfe16506327fc64d949d32c41177611b7
HelloThere-ImMat <dorr.mathias@gmail.com>
2023-05-08 14:31:00 +0200

    EXIIIIT SUUUUCESSSSSSSSS

--- srcs/execution/builtin/builtin_fcts/env.c
+++ srcs/execution/builtin/builtin_fcts/env.c
@@ -6,7 +6,7 @@
 /*   By: mat <mat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 09:36:04 by mat               #+#    #+#             */
-/*   Updated: 2023/05/08 10:03:58 by mat              ###   ########.fr       */
+/*   Updated: 2023/05/08 14:30:45 by mat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ int	env_builtin(t_command *cmd_data)
 	index = 0;
 	while (cmd_data->env[index] != NULL)
 		printf("%s\n", cmd_data->env[index++]);
-	return (0);
+	return (EXIT_SUCCESS);
 }
