5cecdd7783d74fc6ea26e67b46fdb74b9a87b21f
romann-broque <kinoide56@gmail.com>
2023-05-28 14:38:30 +0200

    fix cd with OLDPWD then cd -

--- srcs/execution/builtin/builtin_fcts/cd.c
+++ srcs/execution/builtin/builtin_fcts/cd.c
@@ -6,16 +6,41 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 00:36:27 by mat               #+#    #+#             */
-/*   Updated: 2023/05/28 13:17:38 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/05/28 14:38:00 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	handle_chdir_err(char *const cd_arg)
+static void	handle_chdir_err(const char *const cd_arg)
 {
 	print_error("%s: %s: %s: %s\n",
 		MINISHELL, CD_BUILTIN, cd_arg, strerror(errno));
+}
+
+static int	change_dir(const char *arg, const bool is_print)
+{
+	int	ret_val;
+
+	ret_val = EXIT_FAILURE;
+	if (arg[0] == '\0' || chdir(arg) != -1)
+	{
+		if (arg[0] == '\0')
+		{
+			ft_printf("%s\n", ft_getenv(OLDPWD_VAR));
+			update_cwd_var(arg);
+		}
+		else
+		{
+			update_cwd_var(arg);
+			if (is_print == true)
+				print_pos();
+		}
+		ret_val = EXIT_SUCCESS;
+	}
+	else
+		handle_chdir_err(arg);
+	return (ret_val);
 }
 
 static int	execute_cd(t_command *cmd_data)
@@ -28,17 +53,7 @@ static int	execute_cd(t_command *cmd_data)
 	ret_val = EXIT_FAILURE;
 	check_pos(CHDIR);
 	if (cd_arg != NULL)
-	{
-		if (chdir(cd_arg) != -1)
-		{
-			update_cwd_var(cd_arg);
-			if (is_print == true)
-				print_pos();
-			ret_val = EXIT_SUCCESS;
-		}
-		else
-			handle_chdir_err(cd_arg);
-	}
+		ret_val = change_dir(cd_arg, is_print);
 	free(cd_arg);
 	is_print = false;
 	return (ret_val);
