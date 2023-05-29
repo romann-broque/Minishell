29b72a3ee7e5888dba5aeb69f126d9610adad1a5
romann-broque <kinoide56@gmail.com>
2023-05-29 17:39:07 +0200

    fix compil

--- srcs/execution/builtin/cwd/cwd_utils.c
+++ srcs/execution/builtin/cwd/cwd_utils.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 16:15:51 by rbroque           #+#    #+#             */
-/*   Updated: 2023/05/29 17:34:11 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/05/29 17:37:14 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,8 +48,10 @@ void	check_pos(const char *caller)
 
 static int	print_pos_exec(const char *caller, char *cwd, char *pos)
 {
-	char		*printed_pos;
+	char	*printed_pos;
+	int		ret_val;
 
+	ret_val = EXIT_SUCCESS;
 	if (cwd != NULL && is_cmd_accessible(cwd) == true)
 		printed_pos = cwd;
 	else
@@ -58,26 +60,29 @@ static int	print_pos_exec(const char *caller, char *cwd, char *pos)
 	{
 		print_error("%s: %s: %s: %s\n",
 			MINISHELL, caller, WRITE_ERROR, strerror(errno));
-		return (EXIT_FAILURE);
+		ret_val = EXIT_FAILURE;
 	}
+	return (ret_val);
 }
 
 int	print_pos(const char *caller)
 {
 	char *const	cwd = ft_getenv(PWD_VAR);
 	char		*pos;
+	int			ret_val;
 
 	pos = getcwd(NULL, 0);
 	if (pos != NULL)
-		print_pos_exec(caller, cwd, pos);
+		ret_val = print_pos_exec(caller, cwd, pos);
 	else
 	{
 		print_error("%s: %s: %s: %s: %s\n",
 			PWD_BUILTIN, ERROR_ACCESS_DIR, GETCWD,
 			ERROR_ACCESS_PAR_DIR, strerror(errno));
 		free(pos);
+		ret_val = EXIT_FAILURE;
 		return (EXIT_FAILURE);
 	}
 	free(pos);
-	return (EXIT_SUCCESS);
+	return (ret_val);
 }
