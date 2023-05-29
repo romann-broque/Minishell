989a247261bccc0729e888c7f3377d43d6638d80
romann-broque <kinoide56@gmail.com>
2023-05-29 15:13:18 +0200

    fix redirection errors behaviors

--- srcs/redirection/redirection.c
+++ srcs/redirection/redirection.c
@@ -6,11 +6,16 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 11:12:14 by rbroque           #+#    #+#             */
-/*   Updated: 2023/05/28 15:47:08 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/05/29 15:08:59 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static bool	are_redirection_valid(t_command *cmd)
+{
+	return (cmd->fdin != INVALID_FD && cmd->fdout != INVALID_FD);
+}
 
 static void	change_output(
 	t_command *curr_cmd,
@@ -20,13 +25,16 @@ static void	change_output(
 {
 	if (curr_cmd->fdout != STDOUT_FILENO)
 		close_safe(curr_cmd->fdout);
-	if (tok_redirect->type == T_IDLE)
+	if (are_redirection_valid(curr_cmd) == true)
 	{
-		print_error("%s: %s\n", MINISHELL, AMB_REDIRECT);
-		curr_cmd->fdin = INVALID_FD;
+		if (tok_redirect->type == T_IDLE)
+		{
+			print_error("%s: %s\n", MINISHELL, AMB_REDIRECT);
+			curr_cmd->fdin = INVALID_FD;
+		}
+		else
+			curr_cmd->fdout = get_out_fd(tok_redirect->value, tok_type);
 	}
-	else
-		curr_cmd->fdout = get_out_fd(tok_redirect->value, tok_type);
 }
 
 static void	change_input(
@@ -37,13 +45,16 @@ static void	change_input(
 {
 	if (curr_cmd->fdin != STDIN_FILENO)
 		close_safe(curr_cmd->fdin);
-	if (tok_redirect->type == T_IDLE)
+	if (are_redirection_valid(curr_cmd) == true)
 	{
-		print_error("%s: %s\n", MINISHELL, AMB_REDIRECT);
-		curr_cmd->fdin = INVALID_FD;
+		if (tok_redirect->type == T_IDLE)
+		{
+			print_error("%s: %s\n", MINISHELL, AMB_REDIRECT);
+			curr_cmd->fdin = INVALID_FD;
+		}
+		else
+			curr_cmd->fdin = get_in_fd(tok_redirect->value, tok_type);
 	}
-	else
-		curr_cmd->fdin = get_in_fd(tok_redirect->value, tok_type);
 }
 
 void	update_fds(t_toktype toktype, t_token *tok, t_command *cmd)
